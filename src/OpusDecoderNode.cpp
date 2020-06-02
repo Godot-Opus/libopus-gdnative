85da90e463af21b8e121c87a76d9d55c225073c0
Adam Brown <adamwbrown@gmail.com>
2020-06-01 22:49:57 -0700

    A ton of copde clean up
    Also working on the upsampler

--- src/OpusDecoderNode.cpp
+++ src/OpusDecoderNode.cpp
@@ -50,39 +50,22 @@ PoolByteArray OpusDecoderNode::decode(const PoolByteArray &opusEncoded)
 	int numInputBytes = opusEncoded.size();
 	const unsigned char *compressedBytes = opusEncoded.read().ptr();
 
-	const int channel_width = 2;
-	// $TODO: max_frame_size here in incorrect, that is a PCM value.
-	unsigned char *inBuff = new unsigned char[max_frame_size*channels*channel_width];
-	unsigned char *outPcmBytes = new unsigned char[max_frame_size*channels*channel_width];
-	opus_int16 *pcmSamples = new int16_t[max_frame_size];
-	opus_int16 *out = new int16_t[max_frame_size];
+	opus_int16 *out = new opus_int16[max_frame_size];
 
 	int byteMark = 0;
-
-	Godot::print("Setup complete.");
-
-	int outBytes = 0;
 
 	bool done = false;
 	while(!done)
 	{
 		// Clear the buffers
-		memset(inBuff, 0, sizeof(unsigned char)*max_frame_size*channels*channel_width);
-		memset(pcmSamples, 0, sizeof(opus_int16)*max_frame_size);
 		memset(out, 0, sizeof(opus_int16)*max_frame_size);
-		memset(outPcmBytes, 0, sizeof(unsigned char)*max_frame_size*channels*channel_width);
 
 		// Parse out packet size
-		//Godot::print("Parsing packet size...");
 		Bytes4 b{0};
 		for(int ii=0; ii<4; ++ii) b.bytes[ii] = compressedBytes[byteMark+ii];
 		int packetSize = b.integer;
 
 		byteMark += 4; // Move past the packet size
-
-		//Godot::print("Decode packetSize: {0}", packetSize);
-		//Godot::print("byteMark: {0}", byteMark);
-		//Godot::print("opusEncoded: {0}", numInputBytes);
 
 		if(packetSize <= 0 || packetSize > 2048)
 		{
@@ -90,13 +73,7 @@ PoolByteArray OpusDecoderNode::decode(const PoolByteArray &opusEncoded)
 			break;
 		}
 
-		// Copy packet into buffer
-		/*
-		for(int ii=0; ii<packetSize; ++ii)
-		{
-			inBuff[ii] = compressedBytes[byteMark+ii];
-		}
-		*/
+		// Get pointer to current packet
 		const unsigned char *inData = &compressedBytes[byteMark];
 
 		byteMark += packetSize; // move past the packet
@@ -107,8 +84,6 @@ PoolByteArray OpusDecoderNode::decode(const PoolByteArray &opusEncoded)
 			done = true;
 		}
 
-		//Godot::print("opus size: {0} byteMark: {1}\n", opusEncoded.size(), byteMark);
-
 		// Decode the current opus packet
 		int out_frame_size = opus_decode(decoder, inData, packetSize, out, max_frame_size, 0);
 		if(out_frame_size < 0)
@@ -117,72 +92,14 @@ PoolByteArray OpusDecoderNode::decode(const PoolByteArray &opusEncoded)
 			break;
 		}
 
-		outBytes += out_frame_size * channels * channel_width;
-
-		//Godot::print("out_frame_size: {0}", out_frame_size);
-
-		/*
-		// Convert decoded data to little-endian
-		for(int i = 0; i < frame_size*channels*channel_width; i++)
-		{
-			//Godot::print("out sample: {0}", out[i]);
-			Bytes2 b2{};
-			b2.integer = out[i];
-
-			decodedPcm.append(b2.bytes[0]);
-			decodedPcm.append(b2.bytes[1]);
-
-//			outPcmBytes[2 * i] = out[i] & 0xFF;
-//			outPcmBytes[2 * i + 1] = (out[i] >> 8) & 0xFF;
-		}
-		*/
-
 		unsigned char *bytes = reinterpret_cast<unsigned char*>(out);
-		for(int ii=0; ii<out_frame_size*channels*channel_width; ++ii)
+		for(int ii=0; ii<out_frame_size*channels*pcm_channel_size; ++ii)
 		{
 			decodedPcm.append(bytes[ii]);
 		}
-
-		// Copy PCM samples into output buffer
-		/*
-		for(int ii = 0; ii<out_frame_size*channels; ++ii)
-		{
-			opus_int16 sample = pcmSamples[ii];
-			//Godot::print("out sample: {0}", sample);
-			//uint8_t* bytes = reinterpret_cast<uint8_t*>(&sample);
-			//uint8_t first = (sample >> (0 * 8)) & 0xFF;
-			//uint8_t second = (sample >> (1 * 8)) & 0xFF;
-			Bytes2 b2{};
-			b2.integer = sample;
-
-			decodedPcm.append(b2.bytes[0]);
-			decodedPcm.append(b2.bytes[1]);
-		}
-		*/
-		/*
-		const uint8_t *pcmBytes = reinterpret_cast<uint8_t *>(pcmSamples);
-		int numPcmBytes = channels * pcm_channel_size * out_frame_size;
-		for(int ii=0; ii<numPcmBytes; ++ii)
-		{
-			decodedPcm.append(pcmBytes[ii]);
-		}
-		*/
-		//Godot::print("pass complete");
 	}
 
-	const opus_int16 *samples = reinterpret_cast<const opus_int16*>(decodedPcm.read().ptr());
-	for(int ii=0; ii<500; ++ii)
-	{
-		Godot::print("out: {0}", samples[ii]);
-	}
-
-	Godot::print("outBytes: {0}", outBytes);
-	//Godot::print("Decoding complete: {0}", decodedPcm.size());
-
-	delete [] inBuff;
-	delete [] pcmSamples;
 	delete [] out;
-	delete [] outPcmBytes;
 
 	return decodedPcm;
 }
--- src/OpusEncoderNode.cpp
+++ src/OpusEncoderNode.cpp
@@ -61,100 +61,62 @@ void OpusEncoderNode::_ready()
 	}
 }
 
+inline double lerp(double v0, double v1, double t) {
+	return (1.0 - t) * v0 + t * v1;
+}
+
 PoolByteArray OpusEncoderNode::resample_441kh_48kh(const PoolByteArray &rawPcm)
 {
 	const float ratio = 160.0f/147.0f;
 	const int numRawBytes = rawPcm.size();
 	const PoolByteArray::Read reader = rawPcm.read();
 	const opus_int16 *readSamples = reinterpret_cast<const opus_int16 *>(reader.ptr());
-	const int numReadSamples = rawPcm.size() / 2;
 
 	const int upsampledSize = (int)floor(numRawBytes * ratio);
 	const int numWriteSamples = upsampledSize / pcm_channel_size;
 	opus_int16 *writableSamples = new opus_int16[numWriteSamples];
 
-	Godot::print("numRawBytes: {0}", numRawBytes);
-	Godot::print("upsampledBytesSize: {0}", upsampledSize);
-//	PoolByteArray upsampled;
-//	upsampled.resize(newSize);
-//	PoolByteArray::Write writable = upsampled.write();
-//	uint16_t *writableSamples = reinterpret_cast<uint16_t *>(writable.ptr());
-//	const int numWriteSamples = newSize / 2;
-
 	const float mappingRatio = 44100.0f/48000.0f;
 
-//	for(int ii=0; ii<1000; ++ii)
-//	{
-//		Godot::print("orig: {0}", readSamples[ii]);
-//	}
-
-	/*
-	for(int ii=0; ii<numReadSamples; ++ii)
-	{
-		Godot::print("org sample: {0}", readSamples[ii]);
-	}
-	*/
-
 	int map44XLast = 0;
-	//for(int xx=0; xx<numWriteSamples; xx+=channels)
+
 	for(int xx=0; xx<numWriteSamples-1; xx+=channels)
 	{
-		int map44X = (int)round((float)xx * mappingRatio);
-		//Godot::print("numReadSamples: {0}", numReadSamples);
-		//Godot::print("map44X: {0} map44XLast: {1}", map44X, map44XLast);
+		//int map44X = (int)round((float)xx * mappingRatio);
+		float smallCord = (float)xx * mappingRatio;
+		double intpart;
+		double percent = (float)modf(smallCord , &intpart);
+		int map44X = (int)intpart;
+
 		if(map44XLast == map44X)
 		{
-			writableSamples[xx] = (uint16_t)(((int)readSamples[map44X] + (int)readSamples[map44XLast]) / 2);
-			//Godot::print("up sample INTER: {0}", writableSamples[xx]);
+			Godot::print("INTERP {0}", map44X);
+			writableSamples[xx] = lerp((double)readSamples[map44XLast], (double)readSamples[map44X], percent);
+			//writableSamples[xx] = (opus_int16)(((float)readSamples[map44X] + (float)readSamples[map44XLast]) / 2.0f);
 			if (channels==2)
 			{
-				writableSamples[xx+1] = (uint16_t)(((int)readSamples[map44X+1] + (int)readSamples[map44XLast+1]) / 2);
-				//Godot::print("up sample INTER2: {0}", writableSamples[xx+1]);
+				writableSamples[xx+1] = lerp((double)readSamples[map44XLast+1],(double)readSamples[map44X+1],  percent);
+				//writableSamples[xx+1] = (opus_int16)(((float)readSamples[map44X+1] + (float)readSamples[map44XLast+1]) / 2.0f);
 			}
 		}
 		else
 		{
-			//Godot::print("up read: {0}", readSamples[map44X]);
-			writableSamples[xx] = (uint16_t)readSamples[map44X];
-			//Godot::print("up sample: {0}", writableSamples[xx]);
+			writableSamples[xx] = readSamples[map44X];
 			if (channels==2)
 			{
-				//Godot::print("up read: {0}", readSamples[map44X+1]);
-				writableSamples[xx+1] = (uint16_t)readSamples[map44X+1];
-				//Godot::print("up sample2: {0}", (uint16_t)writableSamples[xx+1]);
+				writableSamples[xx+1] = readSamples[map44X+1];
 			}
 		}
 
 		map44XLast = map44X;
 	}
 
-//	for(int ii=0; ii<1000; ++ii)
-//	{
-//		Godot::print("up: {0}", writableSamples[ii]);
-//	}
-
-
-
-	//uint8_t *writableSampleBytes = reinterpret_cast<uint8_t *>(writableSamples);
 	PoolByteArray upsampled;
 	//upsampled.resize(upsampledBytesSize);
-	PoolByteArray::Write writer = upsampled.write();
-	for(int ii=0; ii < numWriteSamples; ++ii)
+	unsigned char *writableSampleBytes = reinterpret_cast<unsigned char *>(writableSamples);
+	for(int ii=0; ii < numWriteSamples*pcm_channel_size; ++ii)
 	{
-		//Godot::print("up sample: {0}", writableSampleBytes[ii]);
-		opus_int16 sample = writableSamples[ii];
-		//Godot::print("UP sample: {0}", sample);
-		Bytes2 b{};
-		b.integer = sample;
-		//uint8_t* bytes = reinterpret_cast<uint8_t*>(&sample);
-		//uint8_t first = (sample >> (0 * 8)) & 0xFF;
-		//uint8_t second = (sample >> (1 * 8)) & 0xFF;
-
-		upsampled.append(b.bytes[0]);
-		upsampled.append(b.bytes[1]);
-
-		//upsampled.append(writableSampleBytes[ii]);
-		//writer[ii] = writableSampleBytes[ii];
+		upsampled.append(writableSampleBytes[ii]);
 	}
 
 //	for(int ii=0; ii<1000; ++ii)
@@ -164,7 +126,6 @@ PoolByteArray OpusEncoderNode::resample_441kh_48kh(const PoolByteArray &rawPcm)
 
 	delete [] writableSamples;
 
-
 	return upsampled;
 }
 
@@ -172,40 +133,23 @@ PoolByteArray OpusEncoderNode::encode(const PoolByteArray &rawPcm)
 {
 	PoolByteArray encodedBytes;
 
-	//PoolByteArray upsampled = resample_441kh_48kh(rawPcm);
-	PoolByteArray upsampled = rawPcm;
+	PoolByteArray upsampled = resample_441kh_48kh(rawPcm);
+	//PoolByteArray upsampled = rawPcm;
 
 	const int numPcmBytes = upsampled.size();
 	const unsigned char *pcm_bytes = upsampled.read().ptr();
 
-	const opus_int16 *pcm_samples = reinterpret_cast<const opus_int16 *>(pcm_bytes);
-	for(int ii=0; ii<500; ++ii)
-	{
-		//Godot::print("rdy : {0}", pcm_samples[ii]);
-		Godot::print("rdy: {0}", (pcm_bytes[2 * ii + 1] << 8 | pcm_bytes[2 * ii]));
-	}
-
-	Godot::print("numPcmBytes: {0}", numPcmBytes);
-
 	const int bytesPerSample = pcm_channel_size * channels;
 	Godot::print("bytes per sample: {0}", bytesPerSample);
 
-	//const int frameSizeBytes = frame_size *  bytesPerSample;
 	opus_int16 *inputSamples = new opus_int16[frame_size * channels];
 
 	// This is the temp buffer we encode to
 	unsigned char *outBuff = new unsigned char[sizeof(unsigned char) * MAX_PACKET_SIZE];
 
-	opus_int16 *out2 = new int16_t[max_frame_size*channels];
-	unsigned char *outBuff3 = new unsigned char[sizeof(unsigned char) * max_frame_size * channels * pcm_channel_size];
-
 	const int availableSamples = numPcmBytes / bytesPerSample;
-	//const int availableFrames = availableSamples / frame_size;
 
 	int remainingSamples = availableSamples;
-
-	Godot::print("frame_size: {0}", frame_size);
-	Godot::print("availableSamples: {0}", availableSamples);
 
 	int markPos = 0;
 
@@ -226,27 +170,13 @@ PoolByteArray OpusEncoderNode::encode(const PoolByteArray &rawPcm)
 		}
 
 		// Remember: input_bytes elements are 2 bytes wide, pcm_bytes are 1 byte wide.
-
-
 		// Convert from little-endian ordering. (I think it does nothing if you are already on a little endian system)
 		for(int ii = 0; ii < (curFrameSize * channels); ii++)
 		{
 			inputSamples[ii] = pcm_bytes[markPos + (2 * ii + 1)] << 8 | pcm_bytes[markPos + (2 * ii)];
-			//Godot::print("in sample: {0}", inputSamples[ii]);
 		}
 
 		markPos += (curFrameSize * channels * pcm_channel_size);
-
-//		Godot::print("--------------------");
-//		for(int ii=0; ii<10; ++ii)
-//		{
-//			Godot::print("in: {0}", inputSamples[ii]);
-//		}
-
-//		for(int ii=0; ii<500; ++ii)
-//		{
-//			Godot::print("lst: {0}", inputSamples[ii]);
-//		}
 
 		// Encode the frame.
 		int opusPacketSize = opus_encode(encoder, inputSamples, frame_size, outBuff, MAX_PACKET_SIZE);
@@ -256,37 +186,7 @@ PoolByteArray OpusEncoderNode::encode(const PoolByteArray &rawPcm)
 			break;
 		}
 
-//		int out_frame_size = opus_decode(decoder, outBuff, opusPacketSize, out2, max_frame_size, 0);
-//		if(out_frame_size < 0)
-//		{
-//			Godot::print("decoder failed: {0}", opus_strerror(out_frame_size));
-//			break;
-//		}
-
-		//Godot::print("out_frame_size: {0}", out_frame_size);
-
-		// Convert to little-endian ordering.
-//		for(int i=0;i<channels*out_frame_size;i++)
-//		{
-//			outBuff3[2*i]=out2[i]&0xFF;
-//			outBuff3[2*i+1]=(out2[i]>>8)&0xFF;
-//		}
-//
-//		for(int ii=0; ii<10; ++ii)
-//		{
-//			Godot::print("out: {0}", out2[ii]);
-//		}
-//		Godot::print("--------------------");
-
-		//unsigned char *x = reinterpret_cast<unsigned char *>(out2);
-//		for(int ii = 0; ii < out_frame_size*channels*pcm_channel_size; ++ii)
-//		{
-//			encodedBytes.append(outBuff3[ii]);
-//		}
-
-
 		// Prepend the frame size
-		//Godot::print("Encode packetSize: {0}", opusPacketSize);
 		Bytes4 b{opusPacketSize};
 		for(unsigned char byte : b.bytes) encodedBytes.append(byte);
 
@@ -297,10 +197,8 @@ PoolByteArray OpusEncoderNode::encode(const PoolByteArray &rawPcm)
 			encodedBytes.append(outBuff[ii]);
 		}
 
-
 		// Record that we've processed how ever many frames we processed
 		remainingSamples -= curFrameSize;
-		//Godot::print("remainingSamples: {0}", remainingSamples);
 	}
 
 	delete[] inputSamples;
